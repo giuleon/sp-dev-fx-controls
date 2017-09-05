0c97c5ddd4babc749eefdc8f8fec28b0d341fbab
Elio Struyf <info@estruyf.be>
2017-09-05 17:17:46 +0200

    File type icon component added

--- src/webparts/testFields/components/ITestFieldsProps.ts
+++ src/webparts/testFields/components/ITestFieldsProps.ts
@@ -1,9 +1,15 @@
 import { ICheckedTerms } from '../../../PropertyFieldTermSetPicker';
 import { IDateFieldValue } from "../../../PropertyFieldDatePicker";
+import { IImageSize } from "../../../FileTypeIcon";
+
 export interface ITestFieldsProps {
   description: string;
   list: string | string[];
   multiList: string[];
   terms: ICheckedTerms;
   datetime: IDateFieldValue;
+}
+
+export interface ITestFieldsState {
+  imgSize: IImageSize;
 }
--- src/webparts/testFields/components/TestFields.tsx
+++ src/webparts/testFields/components/TestFields.tsx
@@ -1,10 +1,46 @@
 import * as React from 'react';
 import styles from './TestFields.module.scss';
-import { ITestFieldsProps } from './ITestFieldsProps';
+import { ITestFieldsProps, ITestFieldsState } from './ITestFieldsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { FileTypeIcon, IApplicationType, IIconType, IImageSize } from "../../../FileTypeIcon";
+import { Dropdown, IDropdownOption } from "office-ui-fabric-react/lib/components/Dropdown";
 
-export default class TestFields extends React.Component<ITestFieldsProps, {}> {
+export default class TestFields extends React.Component<ITestFieldsProps, ITestFieldsState> {
+  constructor(props: ITestFieldsProps) {
+    super(props);
+
+    this.state = {
+      imgSize: IImageSize.small
+    };
+
+    this._onChange = this._onChange.bind(this);
+  }
+
+  private _onChange(element?: IDropdownOption): void {
+    this.setState({
+      imgSize: parseInt(element.key.toString())
+    });
+  }
+
   public render(): React.ReactElement<ITestFieldsProps> {
+    const sizeOptions: IDropdownOption[] = [
+      {
+        key: IImageSize.small,
+        text: IImageSize[IImageSize.small],
+        selected: IImageSize.small === this.state.imgSize
+      },
+      {
+        key: IImageSize.medium,
+        text: IImageSize[IImageSize.medium],
+        selected: IImageSize.medium === this.state.imgSize
+      },
+      {
+        key: IImageSize.large,
+        text: IImageSize[IImageSize.large],
+        selected: IImageSize.large === this.state.imgSize
+      }
+    ];
+
     return (
       <div className={styles.testFields}>
         <div className={styles.container}>
@@ -17,6 +53,30 @@ export default class TestFields extends React.Component<ITestFieldsProps, {}> {
               <p className="ms-font-l ms-fontColor-white">Multi List: {this.props.multiList.join(', ')}</p>
               <p className="ms-font-l ms-fontColor-white">Term(s): {this.props.terms.map(t => t.name).join(', ')}</p>
               <p className="ms-font-l ms-fontColor-white">Date: {this.props.datetime.displayValue}</p>
+              <p className="ms-font-l ms-fontColor-white">Font icons:
+                <FileTypeIcon type={IIconType.font} path="https://contoso.sharepoint.com/documents/filename.docx" />
+                <FileTypeIcon type={IIconType.font} path="https://contoso.sharepoint.com/documents/filename.unknown" />
+                <FileTypeIcon type={IIconType.font} path="https://contoso.sharepoint.com/documents/filename.doc" />
+                <FileTypeIcon type={IIconType.font} application={IApplicationType.HTML} />
+                <FileTypeIcon type={IIconType.font} application={IApplicationType.Mail} />
+                <FileTypeIcon type={IIconType.font} application={IApplicationType.SASS} />
+              </p>
+              <p className="ms-font-l ms-fontColor-white">Image icons:
+                <FileTypeIcon type={IIconType.image} path="https://contoso.sharepoint.com/documents/filename.docx" />
+                <FileTypeIcon type={IIconType.image} path="https://contoso.sharepoint.com/documents/filename.unknown" />
+                <FileTypeIcon type={IIconType.image} path="https://contoso.sharepoint.com/documents/filename.doc" />
+                <FileTypeIcon type={IIconType.image} application={IApplicationType.Word} />
+
+                <FileTypeIcon type={IIconType.image} size={IImageSize.small} application={IApplicationType.Excel} />
+                <FileTypeIcon type={IIconType.image} size={IImageSize.medium} application={IApplicationType.Excel} />
+                <FileTypeIcon type={IIconType.image} size={IImageSize.large} application={IApplicationType.Excel} />
+              </p>
+
+              <p className="ms-font-l ms-fontColor-white">Icon size tester:
+                <Dropdown options={sizeOptions} onChanged={this._onChange} />
+                <FileTypeIcon type={IIconType.image} size={this.state.imgSize} application={IApplicationType.Excel} />
+                <FileTypeIcon type={IIconType.image} size={this.state.imgSize} />
+              </p>
             </div>
           </div>
         </div>
